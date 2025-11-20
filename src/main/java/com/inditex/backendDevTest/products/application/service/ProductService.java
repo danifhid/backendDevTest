c5f1c770d678ed6cab43d46e1d55409b8277b03f
danifhid <danifhid@gmail.com>
2025-11-20 09:49:51 +0100

    [Products tests] - Deleted unused import and directory

--- src/main/java/com/inditex/backendDevTest/products/application/service/ProductService.java
+++ src/main/java/com/inditex/backendDevTest/products/application/service/ProductService.java
@@ -2,7 +2,6 @@ package com.inditex.backendDevTest.products.application.service;
 
 import com.inditex.backendDevTest.products.application.dto.ProductDetailDTO;
 import org.apache.coyote.BadRequestException;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
